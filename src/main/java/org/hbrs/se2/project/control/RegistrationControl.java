09b44b72c9035afe42eb777c086277ed8ba9f112
dbloke2s <dominik.blokesch@smail.inf.h-brs.de>
2022-11-01 20:49:44 +0100

    Implemented Factories, Beans, Binder. Proper use of DTOs. Started work on making the registration a bit more generic. - WIP!

--- src/main/java/org/hbrs/se2/project/control/RegistrationControl.java
+++ src/main/java/org/hbrs/se2/project/control/RegistrationControl.java
@@ -1,6 +1,9 @@
 package org.hbrs.se2.project.control;
 
 import org.hbrs.se2.project.control.exception.DatabaseUserException;
+import org.hbrs.se2.project.control.factories.CompanyFactory;
+import org.hbrs.se2.project.control.factories.StudentFactory;
+import org.hbrs.se2.project.control.factories.UserFactory;
 import org.hbrs.se2.project.dtos.CompanyDTO;
 import org.hbrs.se2.project.dtos.StudentDTO;
 import org.hbrs.se2.project.dtos.UserDTO;
@@ -10,10 +13,10 @@ import org.hbrs.se2.project.entities.User;
 import org.hbrs.se2.project.repository.CompanyRepository;
 import org.hbrs.se2.project.repository.StudentRepository;
 import org.hbrs.se2.project.repository.UserRepository;
+import org.hbrs.se2.project.util.Globals;
 import org.hbrs.se2.project.util.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Component
 public class RegistrationControl {
@@ -27,25 +30,18 @@ public class RegistrationControl {
     @Autowired
     private CompanyRepository companyRepository;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    private UserDTO userDTO = null;
-    private StudentDTO student = null;
-    private CompanyDTO company = null;
-
     // register new student by creating an account and a student profile
     // and save account data in user table and student data in student table
-    public boolean registerStudent(User user, Student student) throws DatabaseUserException {
-        this.createAccount(user);
-        // get User id from new saved user in db
-        // so we can assign the userid to the data in student table
-        UserDTO userDTO1 = userRepository.findUserByUsername(user.getUsername());
-        student.setUserid(userDTO1.getUserid());
+    public boolean registerStudent(UserDTO user, StudentDTO student) throws DatabaseUserException {
+        String role = Globals.Roles.student;
+        // create new user of role "student"
+        this.createAccount(user, role);
+        // get User id from new saved user in db so we can assign the userid to the data in student table
+        UserDTO newSavedUser = userRepository.findUserByUsername(user.getUsername());
         // create student profile
         try {
             try {
-                this.createStudentProfile(student);
+                this.createStudentProfile(student, newSavedUser);
             } catch (DatabaseUserException e) {
                 throw new DatabaseUserException("Student Profile could not be created at createStudentProfile");
             }
@@ -54,7 +50,7 @@ public class RegistrationControl {
             // so there does nat exist user data without student data
             // both has to exist
             System.out.println("Student profile could not be saved why user data was deleted.");
-            userRepository.deleteById(userDTO1.getUserid());
+            userRepository.deleteById(newSavedUser.getUserid());
             throw new RuntimeException("Matrikelnumber already exists.");
         }
         return true;
@@ -62,16 +58,16 @@ public class RegistrationControl {
 
     // register new company by creating an account and a company profile
     // and save account data in user table and company data in student table
-    public boolean registerCompany(User user, Company company) throws DatabaseUserException {
-        this.createAccount(user);
+    public boolean registerCompany(UserDTO user, CompanyDTO company) throws DatabaseUserException {
+        String role = Globals.Roles.company;
+        // create new user of role "company"
+        this.createAccount(user, role);
         // get User id from new saved user in db
-        UserDTO userDTO2 = userRepository.findUserByUsername(user.getUsername());
-        company.setUserid(userDTO2.getUserid());
-
+        UserDTO newSavedUser = userRepository.findUserByUsername(user.getUsername());
         // create company profile
         try {
             try {
-                this.createCompanyProfile(company);
+                this.createCompanyProfile(company, newSavedUser);
             } catch (DatabaseUserException e) {
                 throw new DatabaseUserException("Company Profile could not be created at registerCompany");
             }
@@ -80,35 +76,34 @@ public class RegistrationControl {
             // so there does nat exist user data without company data
             // both has to exist
             System.out.println("Company profile could not be saved why user data was deleted.");
-            userRepository.deleteById(userDTO2.getUserid());
+            userRepository.deleteById(newSavedUser.getUserid());
             throw new RuntimeException("Company Profile could not be created at registerCompany");
         }
         return true;
     }
 
-    public UserDTO getCurrentUser() {return this.userDTO;}
-
-    private void createAccount(User user) throws DatabaseUserException {
+    private void createAccount(UserDTO userDTO, String role) throws DatabaseUserException {
         try {
-            //Hashing password
-            user.setPassword(passwordEncoder.encode(user.getPassword()));
-            //Saving user with hashed password in db
-            this.userRepository.save(user);
+            User userEntity = UserFactory.createUser(userDTO, role);
+            //Saving user in db
+            this.userRepository.save(userEntity);
         } catch (org.springframework.dao.DataAccessResourceFailureException e) {
             throw new DatabaseUserException("A Failure occurred while saving a user account in the database at createAccount");
         }
     }
 
-    private void createStudentProfile(Student student) throws DatabaseUserException {
+    private void createStudentProfile(StudentDTO studentDTO, UserDTO userDTO) throws DatabaseUserException {
         try {
+            Student student = StudentFactory.createStudent(studentDTO, userDTO);
             this.studentRepository.save(student);
         } catch(org.springframework.dao.DataAccessResourceFailureException e) {
             throw new DatabaseUserException("A Failure occurred while saving a Student Profile into the database at createStudentProfile");
         }
     }
 
-    private void createCompanyProfile(Company company) throws DatabaseUserException {
+    private void createCompanyProfile(CompanyDTO companyDTO, UserDTO userDTO) throws DatabaseUserException {
         try {
+            Company company = CompanyFactory.createCompany(companyDTO, userDTO);
             this.companyRepository.save(company);
         } catch(org.springframework.dao.DataAccessResourceFailureException e) {
             throw new DatabaseUserException("A Failure occurred while saving a Company Profile into the database at createCompanyProfile");
@@ -116,17 +111,14 @@ public class RegistrationControl {
     }
 
     // makes an array and calls function to check if input is null or empty "" for student
-    public boolean checkFormInputStudent(
-            String username, String password, String email,
-            String firstname, String lastname
-    ) {
-        String[] array = {username, password, email, firstname, lastname};
+    public boolean checkFormInputStudent(UserDTO userDTO, StudentDTO studentDTO) {
+        String[] array = {userDTO.getUsername(), userDTO.getPassword(), userDTO.getEmail(), studentDTO.getFirstname(), studentDTO.getLastname()};
         return Utils.checkIfInputEmpty(array);
     }
 
     // makes array and calls function to check if input is null or emtpy "" for company
-    public boolean checkFormInputCompany(String username, String password, String email, String name) {
-        String[] array = {username, password, email, name};
+    public boolean checkFormInputCompany(UserDTO userDTO, CompanyDTO companyDTO) {
+        String[] array = {userDTO.getUsername(), userDTO.getPassword(), userDTO.getEmail(), companyDTO.getName()};
         return Utils.checkIfInputEmpty(array);
     }
 
---
+++ src/main/java/org/hbrs/se2/project/control/factories/UserFactory.java
@@ -0,0 +1,24 @@
+package org.hbrs.se2.project.control.factories;
+
+import org.hbrs.se2.project.dtos.UserDTO;
+import org.hbrs.se2.project.entities.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class UserFactory {
+
+    public static User createUser (UserDTO userDTO, String role) {
+
+        User user = new User();
+
+        user.setUsername(userDTO.getUsername());
+        user.setRole(role);
+        user.setEmail(userDTO.getEmail());
+        //Hashing password
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+
+        return user;
+
+    }
+}
