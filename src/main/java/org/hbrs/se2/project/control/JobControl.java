9e35e40c1b62d4b220bc881f2ab0e8601f48fa46
akrink2s <alexander.krinke@smail.inf.h-brs.de>
2022-11-04 09:30:00 +0100

    added method descriptions

--- src/main/java/org/hbrs/se2/project/control/JobControl.java
+++ src/main/java/org/hbrs/se2/project/control/JobControl.java
@@ -26,15 +26,20 @@ public class JobControl {
 
     @Autowired
     private JobRepository jobRepository;
-
+    
     public CompanyDTO getCompanyByUserid(int id) {
         return companyRepository.findCompanyByUserid(id);
     }
 
+    /**
+     * Saving a new job to the database.*/
     public void createNewJobPost(JobDTOImpl job) {
         this.jobRepository.save(JobFactory.createJob(job));
     }
 
+    /**
+     * @param keyword The word to search for.
+     * @return List of jobs that contain the keyword in either the job title or the job description.*/
     public List<JobDTOImpl> getJobsMatchingKeyword(String keyword) {
         // empty list jobDTOImpls
         List<JobDTOImpl> jobDTOs = new ArrayList<>();
@@ -51,6 +56,12 @@ public class JobControl {
         return getFilteredJobs(jobDTOs, keyword);
     }
 
+    /**
+     * Search for the given keyword in either the job title or the job description
+     * of the given jobs and return the matching jobs.
+     * @param jobs List of all jobs.
+     * @param keyword The word to search for.
+     * @return List of matching jobs.*/
     private List<JobDTOImpl> getFilteredJobs(List<JobDTOImpl> jobs, String keyword) {
         List<JobDTOImpl> matchingJobs = new ArrayList<>();
         for(JobDTOImpl job : jobs) {
@@ -63,13 +74,22 @@ public class JobControl {
         return matchingJobs;
     }
 
+    /**Makes a List of JobsView.JobDetail from the given List of JobDTOImpl
+     * @param jobs List of JobDTOImpl.
+     * @return List of JobsView.JobDetail*/
     public List<JobsView.JobDetail> getAllJobsData(List<JobDTOImpl> jobs) {
+        //empty list
         List<JobsView.JobDetail> jobsData = new ArrayList<>();
         CompanyDTO companyDTO;
         UserDTO userDTO;
+
+
         for(JobDTOImpl job : jobs) {
+            //get CompanyDTO from company database with company id given in job
             companyDTO = companyRepository.findCompanyByCompanyid(job.getCompanyid());
+            //get UserDTO from user database with user id given in companyDTO
             userDTO = userRepository.findUserByUserid(companyDTO.getUserid());
+            //create new JobDetail and add it to jobsData
             jobsData.add(new JobsView.JobDetail(job.getTitle(), job.getSalary(), job.getDescription(), companyDTO.getName(), userDTO.getEmail()));
         }
         return jobsData;
