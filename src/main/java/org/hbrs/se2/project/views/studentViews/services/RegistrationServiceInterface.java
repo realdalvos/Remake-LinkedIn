e9fa9b59b9eb9c1d2f9b78ace1a6eb5be158336b
Julius Kuster <julius.kuster@smail.inf.h-brs.de>
2022-11-04 13:49:35 +0100

    add old RegistrationControl functions

--- src/main/java/org/hbrs/se2/project/views/studentViews/services/RegistrationServiceInterface.java
+++ src/main/java/org/hbrs/se2/project/views/studentViews/services/RegistrationServiceInterface.java
@@ -1,4 +1,11 @@
 package org.hbrs.se2.project.views.studentViews.services;
 
+import org.hbrs.se2.project.dtos.CompanyDTO;
+import org.hbrs.se2.project.dtos.StudentDTO;
+import org.hbrs.se2.project.dtos.UserDTO;
+
 public interface RegistrationServiceInterface {
+
+    public boolean registerStudent(UserDTO user, StudentDTO student) throws Exception;
+    public boolean registerCompany(UserDTO user, CompanyDTO company) throws Exception;
 }
