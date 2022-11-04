ef424476d6637ce9340cdd96ed0d55d466be8b4c
Julius Kuster <julius.kuster@smail.inf.h-brs.de>
2022-11-04 15:12:06 +0100

    adds javaDoc for the functions

--- src/main/java/org/hbrs/se2/project/control/JobControl.java
+++ src/main/java/org/hbrs/se2/project/control/JobControl.java
@@ -15,20 +15,38 @@ public class JobControl {
     @Autowired
     JobService jobService;
 
+    /**
+     * Get the Company of a User
+     * @param id id of User
+     * @return Company of the User
+     */
     public CompanyDTO getCompanyByUserid(int id) {
         return jobService.getCompanyByUserid(id);
     }
 
+    /**
+     * Creates a new Job and saves it in the database
+     * @param job Job object with new Job
+     */
     public void createNewJobPost(JobDTO job) {
         jobService.createNewJobPost(job);
     }
 
+    /**
+     * Sort for Jobs with a specific keyword
+     * @param keyword The keyword to search for
+     * @return A List of all matching Jobs
+     */
     public List<JobDTO> getJobsMatchingKeyword(String keyword) {
         return jobService.getJobsMatchingKeyword(keyword);
     }
 
 
-
+    /**
+     * Get all the Data of a List of Jobs
+     * @param jobs List of Job Objects
+     * @return List of Jobs with all their Details
+     */
     public List<JobsView.JobDetail> getAllJobsData(List<JobDTO> jobs) {
         return jobService.getAllJobsData(jobs);
     }
