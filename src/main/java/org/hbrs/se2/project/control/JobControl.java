3c3ba12372e6b6bd705eb869fda6dcf31f56aea4
Julius Kuster <julius.kuster@smail.inf.h-brs.de>
2022-11-04 14:17:35 +0100

    implements service class

--- src/main/java/org/hbrs/se2/project/control/JobControl.java
+++ src/main/java/org/hbrs/se2/project/control/JobControl.java
@@ -1,78 +1,36 @@
 package org.hbrs.se2.project.control;
 
-import org.hbrs.se2.project.control.factories.JobFactory;
 import org.hbrs.se2.project.dtos.CompanyDTO;
-import org.hbrs.se2.project.dtos.UserDTO;
-import org.hbrs.se2.project.dtos.impl.JobDTOImpl;
-import org.hbrs.se2.project.entities.Job;
-import org.hbrs.se2.project.repository.CompanyRepository;
-import org.hbrs.se2.project.repository.JobRepository;
-import org.hbrs.se2.project.repository.UserRepository;
+import org.hbrs.se2.project.dtos.JobDTO;
 import org.hbrs.se2.project.views.studentViews.JobsView;
+import org.hbrs.se2.project.views.studentViews.services.impl.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class JobControl {
 
     @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private CompanyRepository companyRepository;
-
-    @Autowired
-    private JobRepository jobRepository;
+    JobService jobService;
 
     public CompanyDTO getCompanyByUserid(int id) {
-        return companyRepository.findCompanyByUserid(id);
+        return jobService.getCompanyByUserid(id);
     }
 
-    public void createNewJobPost(JobDTOImpl job) {
-        this.jobRepository.save(JobFactory.createJob(job));
+    public void createNewJobPost(JobDTO job) {
+        jobService.createNewJobPost(job);
     }
 
-    public List<JobDTOImpl> getJobsMatchingKeyword(String keyword) {
-        // empty list jobDTOImpls
-        List<JobDTOImpl> jobDTOs = new ArrayList<>();
-        // get all jobs
-        List<Job> jobs = jobRepository.findAll();
-        // transform all job entities into jobDTOs
-        JobDTOImpl jobDTO;
-        for(Job job : jobs) {
-            jobDTO = new JobDTOImpl(
-                job.getJobid(), job.getCompanyid(), job.getTitle(), job.getDescription(), job.getSalary()
-            );
-            jobDTOs.add(jobDTO);
-        }
-        return getFilteredJobs(jobDTOs, keyword);
+    public List<JobDTO> getJobsMatchingKeyword(String keyword) {
+        return jobService.getJobsMatchingKeyword(keyword);
     }
 
-    private List<JobDTOImpl> getFilteredJobs(List<JobDTOImpl> jobs, String keyword) {
-        List<JobDTOImpl> matchingJobs = new ArrayList<>();
-        for(JobDTOImpl job : jobs) {
-            if(job.getTitle().toLowerCase().contains(keyword.toLowerCase())) {
-                matchingJobs.add(job);
-            } else if(job.getDescription().toLowerCase().contains(keyword.toLowerCase())) {
-                matchingJobs.add(job);
-            }
-        }
-        return matchingJobs;
-    }
 
-    public List<JobsView.JobDetail> getAllJobsData(List<JobDTOImpl> jobs) {
-        List<JobsView.JobDetail> jobsData = new ArrayList<>();
-        CompanyDTO companyDTO;
-        UserDTO userDTO;
-        for(JobDTOImpl job : jobs) {
-            companyDTO = companyRepository.findCompanyByCompanyid(job.getCompanyid());
-            userDTO = userRepository.findUserByUserid(companyDTO.getUserid());
-            jobsData.add(new JobsView.JobDetail(job.getTitle(), job.getSalary(), job.getDescription(), companyDTO.getName(), userDTO.getEmail()));
-        }
-        return jobsData;
+
+    public List<JobsView.JobDetail> getAllJobsData(List<JobDTO> jobs) {
+        return jobService.getAllJobsData(jobs);
     }
 }
 
