a590e9a054b70cbb2ccab13031db3a82c4b5552d
Julius Kuster <julius.kuster@smail.inf.h-brs.de>
2022-11-04 15:09:55 +0100

    adds javaDoc

--- src/main/java/org/hbrs/se2/project/views/studentViews/services/RegistrationServiceInterface.java
+++ src/main/java/org/hbrs/se2/project/views/studentViews/services/RegistrationServiceInterface.java
@@ -6,6 +6,21 @@ import org.hbrs.se2.project.dtos.UserDTO;
 
 public interface RegistrationServiceInterface {
 
+    /**
+     * Create a new Student and save their profile to the database
+     * @param user User to be registered
+     * @param student Student profile to be created
+     * @return True if successful
+     * @throws Exception Data already exists
+     */
     public boolean registerStudent(UserDTO user, StudentDTO student) throws Exception;
+
+    /**
+     * Create a new Company and save it to the database
+     * @param user User the company belongs to
+     * @param company The new Company to be created
+     * @return True if successful
+     * @throws Exception Data already exists
+     */
     public boolean registerCompany(UserDTO user, CompanyDTO company) throws Exception;
 }
