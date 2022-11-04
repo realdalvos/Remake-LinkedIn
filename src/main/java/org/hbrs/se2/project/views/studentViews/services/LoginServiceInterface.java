a7abd7bf3bfb3dca4d0ef86f41435aefd3a02178
Julius Kuster <julius.kuster@smail.inf.h-brs.de>
2022-11-04 13:49:24 +0100

    add old LoginControl functions

--- src/main/java/org/hbrs/se2/project/views/studentViews/services/LoginServiceInterface.java
+++ src/main/java/org/hbrs/se2/project/views/studentViews/services/LoginServiceInterface.java
@@ -1,4 +1,10 @@
 package org.hbrs.se2.project.views.studentViews.services;
 
+import org.hbrs.se2.project.control.exception.DatabaseUserException;
+import org.hbrs.se2.project.dtos.UserDTO;
+
 public interface LoginServiceInterface {
+    public boolean authenticate(String username, String password) throws DatabaseUserException;
+
+    public UserDTO getCurrentUser();
 }
