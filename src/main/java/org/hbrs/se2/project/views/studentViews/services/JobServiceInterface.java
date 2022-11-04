7e5b1ba9cf6b574d223b1b989f761fd84295c7fd
Julius Kuster <julius.kuster@smail.inf.h-brs.de>
2022-11-04 13:49:10 +0100

    add old JobControl functions

--- src/main/java/org/hbrs/se2/project/views/studentViews/services/JobServiceInterface.java
+++ src/main/java/org/hbrs/se2/project/views/studentViews/services/JobServiceInterface.java
@@ -1,4 +1,14 @@
 package org.hbrs.se2.project.views.studentViews.services;
 
+import org.hbrs.se2.project.dtos.CompanyDTO;
+import org.hbrs.se2.project.dtos.impl.JobDTOImpl;
+import org.hbrs.se2.project.views.studentViews.JobsView;
+
+import java.util.List;
+
 public interface JobServiceInterface {
+    public CompanyDTO getCompanyByUserid(int id);
+    public void createNewJobPost(JobDTOImpl job);
+    public List<JobDTOImpl> getJobsMatchingKeyword(String keyword);
+    public List<JobsView.JobDetail> getAllJobsData(List<JobDTOImpl> jobs);
 }
