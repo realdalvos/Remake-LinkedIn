b115c07ab8525076a09eafafce13fb75cedb1a7d
Julius Kuster <julius.kuster@smail.inf.h-brs.de>
2022-11-04 14:54:29 +0100

    adds javaDoc to functions

--- src/main/java/org/hbrs/se2/project/views/studentViews/services/LoginServiceInterface.java
+++ src/main/java/org/hbrs/se2/project/views/studentViews/services/LoginServiceInterface.java
@@ -4,7 +4,17 @@ import org.hbrs.se2.project.control.exception.DatabaseUserException;
 import org.hbrs.se2.project.dtos.UserDTO;
 
 public interface LoginServiceInterface {
+    /**
+     * Autheticate the user that wants to login in with his cedentials.
+     * @param username the username of the user
+     * @param password the password of the user
+     * @return true if authentication was successfull, false if not.
+     * @throws DatabaseUserException Something went wrong with the Database
+     * */
     public boolean authenticate(String username, String password) throws DatabaseUserException;
 
+    /**
+     * @return last successfully authenticated user as UserDTO
+     * */
     public UserDTO getCurrentUser();
 }
