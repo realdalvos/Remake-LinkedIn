cc3656993251cbc66624cd713557762c9db044e2
jperin2s <Jul-Per+2002>
2022-11-06 23:27:02 +0100

    outsurced if clause to service

--- src/main/java/org/hbrs/se2/project/control/factories/CompanyFactory.java
+++ src/main/java/org/hbrs/se2/project/control/factories/CompanyFactory.java
@@ -3,6 +3,7 @@ package org.hbrs.se2.project.control.factories;
 import org.hbrs.se2.project.dtos.CompanyDTO;
 import org.hbrs.se2.project.dtos.UserDTO;
 import org.hbrs.se2.project.entities.Company;
+import org.hbrs.se2.project.services.impl.CompanyService;
 
 public class CompanyFactory {
 
@@ -12,7 +13,7 @@ public class CompanyFactory {
      @return Company Entity
      */
     public static Company createCompany(CompanyDTO companyDTO, UserDTO userDTO) {
-
+        CompanyService companyService = new CompanyService();
         Company company = new Company();
         // pass parameters from companyDTO to company
         company.setName(companyDTO.getName());
@@ -20,12 +21,7 @@ public class CompanyFactory {
         company.setIndustry(companyDTO.getIndustry());
         company.setCompanyid(companyDTO.getCompanyid());
         company.setBanned(companyDTO.getBanned());
-
-        if(companyDTO.getContactdetails() == null) {
-            company.setContactdetails(userDTO.getEmail());
-        }else{
-            company.setContactdetails(companyDTO.getContactdetails());
-        }
+        company.setContactdetails(companyService.initialEmailChange(companyDTO,userDTO).getContactdetails());
         return company;
 
     }
--- src/main/java/org/hbrs/se2/project/services/impl/CompanyService.java
+++ src/main/java/org/hbrs/se2/project/services/impl/CompanyService.java
@@ -2,7 +2,6 @@ package org.hbrs.se2.project.services.impl;
 
 import org.hbrs.se2.project.dtos.CompanyDTO;
 import org.hbrs.se2.project.dtos.UserDTO;
-import org.hbrs.se2.project.dtos.impl.CompanyDTOImpl;
 import org.hbrs.se2.project.services.CompanyServiceInterface;
 
 public class CompanyService implements CompanyServiceInterface {
