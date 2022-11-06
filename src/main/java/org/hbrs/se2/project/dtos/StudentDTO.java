37d9050c96128fdd98374d369e702e40b7a2579a
Philip Haupt <philip.haupt@smail.inf.h-brs.de>
2022-11-06 23:17:03 +0100

    Added student register tests

--- src/main/java/org/hbrs/se2/project/dtos/StudentDTO.java
+++ src/main/java/org/hbrs/se2/project/dtos/StudentDTO.java
@@ -14,4 +14,18 @@ public interface StudentDTO {
     public String getStudyMajor();
 
     public String getUniversity();
+
+    void setStudentId(int id);
+
+    void setUserid(int userid);
+
+    void setFirstname(String firstname);
+
+    void setLastname(String lastname);
+
+    void setMatrikelnumber(String matrikelnumber);
+
+    void setStudyMajor(String studyMajor);
+
+    public void setUniversity(String university);
 }
