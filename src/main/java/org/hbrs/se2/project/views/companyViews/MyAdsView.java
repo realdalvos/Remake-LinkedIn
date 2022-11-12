61609561fa8d8a09d99d0481ce49c58a4caef7a1
Julius Kuster <julius.kuster@smail.inf.h-brs.de>
2022-11-12 03:48:21 +0100

    add ItemDetailRender for the jobs

--- src/main/java/org/hbrs/se2/project/views/companyViews/MyAdsView.java
+++ src/main/java/org/hbrs/se2/project/views/companyViews/MyAdsView.java
@@ -1,7 +1,11 @@
 package org.hbrs.se2.project.views.companyViews;
 
+import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import org.hbrs.se2.project.control.JobControl;
@@ -28,6 +32,39 @@ public class MyAdsView extends Div {
         grid.addColumn(JobDTO::getTitle).setHeader("Titel").setSortable(true);
         grid.addColumn(JobDTO::getDescription).setHeader("Beschreibung");
         grid.addColumn(JobDTO::getSalary).setHeader("Gehalt").setSortable(true);
+
+        // Detail renderer for more information
+        grid.setItemDetailsRenderer(new ComponentRenderer<>(jobDTO -> {
+            FormLayout layout = new FormLayout();
+
+            // Title field
+            final TextField titleField = new TextField("Titel");
+            titleField.setValue(jobDTO.getTitle());
+            titleField.setReadOnly(true);
+
+            // Description Field (TextArea for more rows)
+            final TextArea descriptionField = new TextArea("Beschreibung");
+            descriptionField.setValue(jobDTO.getDescription());
+            descriptionField.setReadOnly(true);
+
+            // Salary Field
+            final TextField salaryField = new TextField("Gehalt");
+            salaryField.setValue(jobDTO.getSalary());
+            salaryField.setReadOnly(true);
+
+            // Location Field
+            final TextField locationField = new TextField("Ort");
+            locationField.setValue(jobDTO.getLocation());
+            locationField.setReadOnly(true);
+
+            //Add textFields to FormLayout
+            layout.add(titleField);
+            layout.add(descriptionField);
+            layout.add(salaryField);
+            layout.add(locationField);
+
+            return layout;
+        }));
 
         add(grid);
     }
