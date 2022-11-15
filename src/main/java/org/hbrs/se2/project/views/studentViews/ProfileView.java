118aeb1337e902e8bb77aadc947f0ecf8f351ed2
vlad <vlad.cordos@gmail.com>
2022-11-16 00:41:12 +0100

    Grids for majors skills and topics added

--- src/main/java/org/hbrs/se2/project/views/studentViews/ProfileView.java
+++ src/main/java/org/hbrs/se2/project/views/studentViews/ProfileView.java
@@ -5,11 +5,14 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.function.ValueProvider;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.component.textfield.TextField;
 import org.hbrs.se2.project.control.ProfileControl;
+import org.hbrs.se2.project.dtos.StudentHasMajorDTO;
 import org.hbrs.se2.project.dtos.UserDTO;
+import org.hbrs.se2.project.repository.StudentHasMajorRepository;
 import org.hbrs.se2.project.util.Globals;
 import org.hbrs.se2.project.views.AppView;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,33 +43,48 @@ public class ProfileView extends Div {
         // print out all majors of a student in console
         List<String> majors = profileControl.getMajorOfStudent(this.getCurrentUser().getUserid());
 
-        Grid<String> grid = new Grid<>();
-        grid.getColumns().forEach(stringColumn -> stringColumn.setAutoWidth(true));
-        grid.recalculateColumnWidths();
-        grid.setItems(majors);
-        for (Object o : majors) {
+        Grid<String> gridMajors = new Grid<>();
+        gridMajors.addColumn(ValueProvider.identity());
+        gridMajors.setItems(majors);
+        add(gridMajors);
 
+
+
+
+        for (Object o : majors) {
+            //gridMajors.addColumn(ValueProvider.identity());
             System.out.println(o);
         }
-        
+
+
 
         TextField university = new TextField("University");
         university.setValue(profileControl.getUniversityOfStudent(this.getCurrentUser().getUserid()));
 
         TextField topic = new TextField("Topics");
         List<String> topics = profileControl.getTopicOfStudent(this.getCurrentUser().getUserid());
+        Grid<String> gridTopics = new Grid<>();
+        gridTopics.addColumn(ValueProvider.identity());
+        gridTopics.setItems(topics);
+        add(gridTopics);
+
         for (Object o : topics) {
             System.out.println(o);
         }
 
         TextField skill = new TextField("Skills");
         List<String> skills = profileControl.getSkillOfStudent(this.getCurrentUser().getUserid());
+        Grid<String> gridSkills = new Grid<>();
+        gridSkills.addColumn(ValueProvider.identity());
+        gridSkills.setItems(skills);
+        add(gridSkills);
+
         for (Object o : skills){
             System.out.println(o);
         }
 
         FormLayout formLayout =  new FormLayout();
-        formLayout.add(major,university,delete,topic,skill);
+        formLayout.add(major,university,delete, undo,topic,skill);
         formLayout.setResponsiveSteps(new FormLayout.ResponsiveStep("0",1));
         add(formLayout);
         add(save);
@@ -75,7 +93,6 @@ public class ProfileView extends Div {
             String universitySaved = profileControl.getUniversityOfStudent(this.getCurrentUser().getUserid());
             profileControl.updateUniversity("", this.getCurrentUser().getUserid());
             university.setValue(profileControl.getUniversityOfStudent(this.getCurrentUser().getUserid()));
-            add(undo);
             undo.addClickListener(buttonClickEvent1 -> {
                 profileControl.updateUniversity(universitySaved, this.getCurrentUser().getUserid());
                 university.setValue(universitySaved);
