4f68e26b02b310e61a948a9f949308a07c0d0ed3
akrink2s <alexander.krinke@smail.inf.h-brs.de>
2022-11-25 21:37:12 +0100

    First working attempt for a access control. Routes, which should be accessible by one group only, get registered after login only.

--- src/main/java/org/hbrs/se2/project/services/impl/LoginService.java
+++ src/main/java/org/hbrs/se2/project/services/impl/LoginService.java
@@ -1,9 +1,20 @@
 package org.hbrs.se2.project.services.impl;
 
+import com.vaadin.flow.component.html.Main;
+import com.vaadin.flow.router.RouteConfiguration;
 import org.hbrs.se2.project.control.exception.DatabaseUserException;
 import org.hbrs.se2.project.dtos.UserDTO;
 import org.hbrs.se2.project.repository.UserRepository;
 import org.hbrs.se2.project.services.LoginServiceInterface;
+import org.hbrs.se2.project.util.Globals;
+import org.hbrs.se2.project.views.AppView;
+import org.hbrs.se2.project.views.LoginView;
+import org.hbrs.se2.project.views.companyViews.MyAdsView;
+import org.hbrs.se2.project.views.companyViews.NewJobAdView;
+import org.hbrs.se2.project.views.companyViews.RegisterCompanyView;
+import org.hbrs.se2.project.views.studentViews.JobsView;
+import org.hbrs.se2.project.views.studentViews.ProfileView;
+import org.hbrs.se2.project.views.studentViews.RegisterStudentView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -22,11 +33,34 @@ public class LoginService implements LoginServiceInterface {
     public boolean authenticate(String username, String password) throws DatabaseUserException {
         UserDTO tmpUser = this.getUserWithJPA(username);
 
-        if(tmpUser == null || !passwordEncoder.matches(password, tmpUser.getPassword().trim())) {
+        if (tmpUser == null || !passwordEncoder.matches(password, tmpUser.getPassword().trim())) {
             return false;
         }
         this.userDTO = tmpUser;
+        createRoutes(tmpUser);
         return true;
+    }
+
+    private void createRoutes(UserDTO user) {
+        RouteConfiguration configuration = RouteConfiguration
+                .forSessionScope();
+
+        Class defaultView;
+        if(user.getRole().equals(Globals.Roles.company)){  //If company
+            configuration.setAnnotatedRoute(MyAdsView.class);
+            configuration.setAnnotatedRoute(NewJobAdView.class);
+            defaultView = MyAdsView.class;
+        }
+        else {    //else student
+            configuration.setAnnotatedRoute(JobsView.class);
+            configuration.setAnnotatedRoute(ProfileView.class);
+            defaultView = JobsView.class;
+        }
+        configuration.setAnnotatedRoute(AppView.class);
+        /*
+        configuration.setRoute(Globals.Pages.LOGIN_VIEW, defaultView, AppView.class);
+        configuration.setRoute(Globals.Pages.REGISTER_COMPANY_VIEW, defaultView, AppView.class);
+        configuration.setRoute(Globals.Pages.REGISTER_STUDENT_VIEW, defaultView, AppView.class);*/
     }
 
     @Override
