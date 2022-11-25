c882f70e78e4b8fb3041248ed549474e0622f002
akrink2s <alexander.krinke@smail.inf.h-brs.de>
2022-11-25 23:11:49 +0100

    Replaced navigateToLoginPage with navigateToDefaultPage.

--- src/main/java/org/hbrs/se2/project/views/RegisterView.java
+++ src/main/java/org/hbrs/se2/project/views/RegisterView.java
@@ -30,7 +30,7 @@ public abstract class RegisterView extends VerticalLayout {
 
     protected Button loginButton() {
         Button loginButton = new Button("Zum Login");
-        loginButton.addClickListener(event -> navigateHandler.navigateToLoginPage());
+        loginButton.addClickListener(event -> navigateHandler.navigateToDefaultPage());
         return loginButton;
     }
 
