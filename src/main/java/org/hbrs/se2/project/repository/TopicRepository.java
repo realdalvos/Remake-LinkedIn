9f686121c121ca95a3f4a538477193f511cd9bdd
dbloke2s <dominik.blokesch@smail.inf.h-brs.de>
2022-11-27 20:07:58 +0100

    Unused imports...

--- src/main/java/org/hbrs/se2/project/repository/TopicRepository.java
+++ src/main/java/org/hbrs/se2/project/repository/TopicRepository.java
@@ -3,10 +3,7 @@ package org.hbrs.se2.project.repository;
 import org.hbrs.se2.project.dtos.TopicDTO;
 import org.hbrs.se2.project.entities.Topic;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public interface TopicRepository extends JpaRepository<Topic, Integer> {
