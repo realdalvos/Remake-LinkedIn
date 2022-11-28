b753af83ef42675fa7a3ed84880a70df31003d28
dbloke2s <dominik.blokesch@smail.inf.h-brs.de>
2022-11-28 10:11:39 +0100

    Moved introduction text to CommonUIElementProvider to remove duplicate code

--- src/main/java/org/hbrs/se2/project/views/companyViews/SearchStudentsView.java
+++ src/main/java/org/hbrs/se2/project/views/companyViews/SearchStudentsView.java
@@ -1,26 +1,22 @@
 package org.hbrs.se2.project.views.companyViews;
 
 import com.vaadin.flow.component.Key;
-import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.grid.ColumnTextAlign;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import org.hbrs.se2.project.control.ProfileControl;
 import org.hbrs.se2.project.dtos.*;
+import org.hbrs.se2.project.services.ui.CommonUIElementProvider;
 import org.hbrs.se2.project.util.Globals;
-import org.hbrs.se2.project.util.Utils;
 import org.hbrs.se2.project.views.AppView;
 
 import java.util.stream.Stream;
@@ -31,18 +27,19 @@ import java.util.stream.Stream;
 @Route(value = Globals.Pages.SEARCH_STUDENT_VIEW, layout = AppView.class, registerAtStartup = false)
 @PageTitle("Student")
 public class SearchStudentsView extends Div {
-    Label emptyLabel = new Label();
+    CommonUIElementProvider ui;
     private final TextField searchField = new TextField();
     private final Button searchButton = new Button("Suchen");
     private final Grid<StudentDTO> grid = new Grid<>();
-    private Grid<MajorDTO> gridMajors = new Grid<>();
-    private Grid<TopicDTO> gridTopics = new Grid<>();
-    private Grid<SkillDTO> gridSkills = new Grid<>();
+    private Grid<MajorDTO> gridMajors;
+    private Grid<TopicDTO> gridTopics;
+    private Grid<SkillDTO> gridSkills;
     private final HorizontalLayout layout = new HorizontalLayout();
     private final HorizontalLayout topLayout = new HorizontalLayout();
     private final ProfileControl profileControl;
 
-    public SearchStudentsView(ProfileControl profileControl) {
+    public SearchStudentsView(ProfileControl profileControl, CommonUIElementProvider ui) {
+        this.ui = ui;
         this.profileControl = profileControl;
 
         grid.setItemDetailsRenderer(new ComponentRenderer<>(student -> {
@@ -71,40 +68,14 @@ public class SearchStudentsView extends Div {
             return formLayout;
         }));
 
-        this.createLayouts();
-        add(introductionText());
+        createLayouts();
+        /*
+        with introductionText() below the career page will show information and help the user
+        guide through the option of either filtering the job ads or showing all available job ads
+        */
+        add(ui.introductionText("Finden Sie hier Ihren zukünftigen Angestellten", "Finden Sie Studenten mit passenden Skills, Themen, Studiengang und Universität"));
         add(topLayout);
         add(grid);
-    }
-
-    /*
-    with introductionText() below the career page will show information and help the user
-    guide through the option of either filtering the job ads or showing all available job ads
-    */
-    private VerticalLayout introductionText() {
-        VerticalLayout vLayout = new VerticalLayout();
-        H2 startText = new H2("Finden Sie hier Ihren zukünftigen Angestellten");
-
-        H3 descriptionText = new H3("Finden Sie Studenten mit passenden Skills, Themen, Studiengang und Universität");
-
-        vLayout.setSizeFull();
-        vLayout.setPadding(false);
-        vLayout.setSpacing(true);
-        vLayout.getThemeList().set("spacing-s", true);
-        vLayout.setAlignItems(FlexComponent.Alignment.STRETCH);
-
-        startText.getElement().getStyle().set("font-size","45px"); // font size
-        startText.getElement().getStyle().set("color", "#f2a6b4"); // hex value of color in custom Theme for continuity
-        startText.getElement().getStyle().set("text-align","center"); // text is now in center
-
-        descriptionText.getElement().getStyle().set("font-size","20px");
-        descriptionText.getElement().getStyle().set("text-align","center");
-
-        vLayout.add(startText);
-        vLayout.add(descriptionText);
-        vLayout.add(emptyLabel);
-
-        return vLayout;
     }
 
     private void createGrids(StudentDTO student) {
@@ -149,7 +120,7 @@ public class SearchStudentsView extends Div {
         topLayout.add(searchButton);
 
         layout.add(topLayout);
-        layout.add(emptyLabel);
+        layout.add(new Label());
         layout.setWidth("100%");
 
         grid.addColumn(StudentDTO::getFirstname).setHeader("Vorname").setTextAlign(ColumnTextAlign.CENTER);
