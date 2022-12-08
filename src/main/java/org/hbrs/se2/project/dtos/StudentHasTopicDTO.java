5228a1f2ec499f061f5c157d390855d43f849bef
dbloke2s <dominik.blokesch@smail.inf.h-brs.de>
2022-12-08 23:19:12 +0100

    Removed now obsolete classes

--- src/main/java/org/hbrs/se2/project/dtos/StudentHasTopicDTO.java
+++
@@ -1,9 +0,0 @@
-package org.hbrs.se2.project.dtos;
-
-public interface StudentHasTopicDTO {
-
-    public int getStudentid();
-    public void setStudentid(int studentid);
-    public int getTopicid();
-    public void setTopicid(int topicid);
-}
