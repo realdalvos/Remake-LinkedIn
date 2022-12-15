8c4524e887afd051924e1e2a235bdcb83b1f6c0e
dbloke2s <dominik.blokesch@smail.inf.h-brs.de>
2022-12-15 09:48:23 +0100

    Added a cookie consent banner...

--- src/main/java/org/hbrs/se2/project/views/LoginView.java
+++ src/main/java/org/hbrs/se2/project/views/LoginView.java
@@ -1,16 +1,20 @@
 package org.hbrs.se2.project.views;
 
 import com.vaadin.flow.component.UI;
-import com.vaadin.flow.component.avatar.Avatar;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.login.LoginForm;
 import com.vaadin.flow.component.login.LoginI18n;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.VaadinRequest;
 import com.vaadin.flow.server.VaadinService;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
@@ -21,7 +25,9 @@ import org.hbrs.se2.project.helper.AccessHandler;
 import org.hbrs.se2.project.helper.navigateHandler;
 import org.hbrs.se2.project.services.ui.CommonUIElementProvider;
 import org.hbrs.se2.project.util.Globals;
-
+import javax.servlet.http.Cookie;
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Show Login View to user when not logged in
@@ -57,10 +63,22 @@ public class LoginView extends VerticalLayout {
 
     public LoginView(LoginControl loginControl, CommonUIElementProvider ui) {
         AccessHandler.setDefaultAccess();
-        setSizeFull();
-        Avatar avatarBasic = new Avatar();
+        setHeight("80%");
         LoginForm component = new LoginForm();
         component.setI18n(createLoginI18n());
+
+        AtomicBoolean understood = new AtomicBoolean(false);
+        Cookie[] cookies = VaadinRequest.getCurrent().getCookies();
+        if (cookies != null) {
+            Arrays.stream(cookies).forEach(cookie -> {
+                if (cookie.getName().equals("understood") && cookie.getValue().equals("true")) {
+                    understood.set(true);
+                }
+            });
+        }
+        if (!understood.get()) {
+            cookieBanner();
+        }
 
         // buttons
         Button buttonStudent = new Button(getTranslation("view.login.button.student"));
@@ -107,11 +125,11 @@ public class LoginView extends VerticalLayout {
         buttonStudent.addClickListener(event -> navigateHandler.navigateToRegisterStudentPage());
         buttonCompany.addClickListener(event -> navigateHandler.navigateToRegisterCompanyPage());
         // add components to View
-        add(avatarBasic);
         add(component);
         add(buttonStudent);
         add(buttonCompany);
         this.setAlignItems(Alignment.CENTER);
+        this.setJustifyContentMode(JustifyContentMode.CENTER);
 
         this.loginControl = loginControl;
         this.ui = ui;
@@ -121,5 +139,25 @@ public class LoginView extends VerticalLayout {
         UserDTO userDTO = loginControl.getCurrentUser();
         UI.getCurrent().getSession().setAttribute(Globals.CURRENT_USER, userDTO);
     }
-}
 
+    private void cookieBanner() {
+        Notification notification = new Notification();
+        notification.setPosition(Notification.Position.BOTTOM_STRETCH);
+        Span text = new Span("Diese Website verwendet für die technische Funktionalität notwendige Cookies.");
+        Anchor link = new Anchor("https://www.cookiesandyou.com/", "Erfahre mehr über Cookies");
+        Button consent = new Button("Verstanden!");
+        consent.addClickListener(event -> {
+            Cookie cookie = new Cookie("understood", "true");
+            cookie.setPath("/");
+            VaadinService.getCurrentResponse().addCookie(cookie);
+            notification.close();
+        });
+        HorizontalLayout info = new HorizontalLayout(text, link, consent);
+        info.setWidthFull();
+        info.setVerticalComponentAlignment(Alignment.CENTER, text, link);
+        consent.getElement().getStyle().set("margin-left", "auto");
+        notification.add(info);
+        notification.open();
+    }
+
+}
