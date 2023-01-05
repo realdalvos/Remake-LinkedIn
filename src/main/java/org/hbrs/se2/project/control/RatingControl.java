c3350f93bd7d41def8cebb1547a0c9acb93259d0
dbloke2s <dominik.blokesch@smail.inf.h-brs.de>
2023-01-05 10:43:12 +0100

    Cleanup

--- src/main/java/org/hbrs/se2/project/control/RatingControl.java
+++ src/main/java/org/hbrs/se2/project/control/RatingControl.java
@@ -1,6 +1,5 @@
 package org.hbrs.se2.project.control;
 
-import com.vaadin.flow.component.html.Span;
 import org.hbrs.se2.project.dtos.RatingDTO;
 import org.hbrs.se2.project.services.impl.RatingService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +11,7 @@ public class RatingControl {
     @Autowired
     RatingService ratingService;
 
-    public Span getRating(int companyid){
+    public Float getRating(int companyid){
         return ratingService.getRating(companyid);
     }
 
